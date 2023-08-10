4cf1cd74a73ddab48c7724428df9ebc74d3093a3
lukivan <lukivan888@gmail.com>
2023-08-10 22:54:41 +0600

    Установил SASS. Реализовал Navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,28 @@
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <nav className="nav-wrapper">
+      <div className="brand">
+        <img width={22} height={22} src="/icon.svg" />
+        <p>
+          service <span>energy</span>
+        </p>
+      </div>
+      <div className="navigation">
+        <div className="links">
+          <a href="/">About</a>
+          <div>
+            Services <img src="/triangle.svg" alt="" />
+          </div>
+        </div>
+        <button>
+          <p>Contact Us</p>
+          <img src="/arrow.svg" alt="" />
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
